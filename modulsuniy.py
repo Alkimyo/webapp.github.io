d024f499e796200caa85f9fa4f3c78faf72bb6fd
Alkimyo <164453140+Alkimyo@users.noreply.github.com>
2024-11-22 19:34:15 +0500

    Add files via upload

--- modulsuniy.py
+++ modulsuniy.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import pickle
 
-# Flask ilovasini yaratish
 app = Flask(__name__)
 
 # Modelni yuklash
@@ -11,30 +10,24 @@ with open('/home/shohruh/Downloads/model.pkl', 'rb') as f:
 # Asosiy sahifa
 @app.route('/')
 def home():
-    return "Bosh sahifa: Ushbu dastur model yordamida bashorat qiladi."
+    return render_template('index.html')
 
 # Bashorat qilish uchun endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        # Foydalanuvchi kiritgan qiymatlarni olish
         age = float(request.form['age'])
         sex = int(request.form['sex'])
         bp = float(request.form['bp'])
         cholesterol = int(request.form['cholesterol'])
         na_to_k = float(request.form['na_to_k'])
 
-        # Model uchun xususiyatlar
         features = [age, sex, bp, cholesterol, na_to_k]
-        
-        # Modeldan bashorat qilish
         prediction = model.predict([features])
-        
-        # Bashorat natijasini qaytarish
-        return f"Bashorat natijasi: {int(prediction[0])}"
-    except Exception as e:
-        return f"Xato yuz berdi: {e}"
 
-# Flask ilovasini ishga tushirish
+        return render_template('index.html', prediction=f"Bashorat natijasi: {int(prediction[0])}")
+    except Exception as e:
+        return render_template('index.html', prediction=f"Xato yuz berdi: {e}")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5005)
